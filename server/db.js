7a93ff3fbe93171a597a4b273a7ff27322d9c057
Tin <overcome7520@gmail.com>
2023-08-09 22:39:25 +0700

    vercel

--- server/db.js
+++ server/db.js
@@ -9,16 +9,21 @@ const { Pool } = require("pg");
 //cú pháp sử dụng:require('dotenv').config();
 require("dotenv").config();
 
+// const pool = new Pool({
+//   user: process.env.USERNAME,
+//   host: process.env.HOST,
+//   database: process.env.DATABASE,
+//   password: process.env.PASSWORD,
+//   port: process.env.DBPORT,
+// });
+
 const pool = new Pool({
-  user: process.env.USERNAME,
-  host: process.env.HOST,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  port: process.env.DBPORT,
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 });
 
-// const pool = new Pool({
-//   connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-// });
+pool.connect((err) => {
+  if (err) throw err;
+  console.log("Connect to PostgreSQL successfully");
+});
 
 module.exports = pool;
