5136d7dd91e7f556cd45851e6836ea367c659ceb
Tin <overcome7520@gmail.com>
2023-08-09 18:48:35 +0700

    fix 2!

--- server/db.js
+++ server/db.js
@@ -9,12 +9,16 @@ const { Pool } = require("pg");
 //cú pháp sử dụng:require('dotenv').config();
 require("dotenv").config();
 
+// const pool = new Pool({
+//   user: process.env.USERNAME,
+//   host: process.env.HOST,
+//   database: process.env.DATABASE,
+//   password: process.env.PASSWORD,
+//   port: process.env.DBPORT,
+// });
+
 const pool = new Pool({
-  user: process.env.USERNAME,
-  host: process.env.HOST,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  port: process.env.DBPORT,
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 });
 
 module.exports = pool;
